221b7caef41adc90417b3e5becd09b278c5df9fa
syedddanishalinaqvi <69369581+syedddanishalinaqvi@users.noreply.github.com>
2024-01-18 13:04:40 +0530

    added public folder

--- middlewares/multer.middleware.js
+++ middlewares/multer.middleware.js
@@ -1,8 +1,14 @@
 import multer from "multer";
+import fs from 'fs';
+
+const destinationDirectory='./image';
+if(!fs.existsSync(destinationDirectory)){
+    fs.mkdirSync(destinationDirectory);
+}
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './images')
+        cb(null, destinationDirectory)
     },
     filename: function (req, file, cb) {
         cb(null, file.originalname)
